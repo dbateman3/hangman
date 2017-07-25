27663e86203502dc9f7b332fac1b22cc3eb880be
Dan <dbateman0319@starkstate.net>
2017-07-24 23:40:30 -0500

    Game fully functional. Recognizes end game values.

--- hangman.rb
+++ hangman.rb
@@ -17,39 +17,51 @@ secret_word = get_secret_word
 
 hidden = secret_word.gsub(/./, "_ ")
 
+puts secret_word
+
 puts hidden
 
 temp_secret_word = secret_word.clone
 
 guesses_left = 6
 
-while guesses_left > 0 
-	puts "Enter a letter:"
-	guess = gets.chomp
+game_over = false
 
-	if secret_word.include?(guess) == true
-		puts "Correct!"
-		incorrect_characters.each do |character|
-			if character == guess
-				incorrect_characters.delete(character)
+while guesses_left > 0 && game_over == false
+	
+		puts "Enter a letter:"
+		guess = gets.chomp
+
+		if secret_word.include?(guess) == true
+			puts "Correct!"
+			incorrect_characters.each do |character|
+				if character == guess
+					incorrect_characters.delete(character)
+				end
+			end
+			guess = guess
+		else
+			puts "Incorrect!"
+			guesses_left = guess_count_down(guesses_left)
+			if guesses_left > 0
+				puts "You have #{guesses_left} incorrect guess(s) left"
+			else
+				puts "GAME OVER!"
 			end
 		end
-		guess = guess
-	else
-		puts "Incorrect!"
-		guesses_left = guess_count_down(guesses_left)
-		if guesses_left > 0
-			puts "You have #{guesses_left} incorrect guess(s) left"
-		else
-			puts "GAME OVER!"
+
+		incorrect_characters.each do |character|
+			temp_secret_word.gsub!(character, "_ ")
 		end
-	end
 
-	incorrect_characters.each do |character|
-		temp_secret_word.gsub!(character, "_ ")
-	end
+		if temp_secret_word.include?("_") == true
+			puts temp_secret_word
+			game_over = false
+		else
+			puts temp_secret_word
+			puts "CONGRATS, YOU WIN!"
+			game_over = true
+		end
 
-	puts temp_secret_word
-
-	temp_secret_word = secret_word.clone
+		temp_secret_word = secret_word.clone
 end
